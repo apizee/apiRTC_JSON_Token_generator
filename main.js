95bdb01c2fc789e19f8974b1ac41b164e7dac597
Kevin <80963524+kmoyse-apizee@users.noreply.github.com>
2021-05-12 10:42:31 +0200

    Update main.js
    Update customizable part to get consistent with blog article and be hopefully more clear

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const jwt = require('jsonwebtoken');
 
 const VERIFY_DELAY = 1000;
 
+// CUSTOMIZE :
 const apiKey = 'YOUR_APIKEY';
 const secret = 'YOUR_SECRET_KEY';
+const userId = "YOUR_USER_ID";
+// OR, To enable Token usage for all your users
+//const userId = "ApiKey_Token";
 
-const apiRTC_UserAgent_Id = "YOUR_USER_ID";
-//const apiRTC_UserAgent_Id = "ApiKey_Token"; //To enable Token usage for all your users
-
-let token = (new AccessToken(apiKey, secret, {apiRTC_UserAgent_Id: apiRTC_UserAgent_Id, ttl: 7200})).toJwt();
+let token = (new AccessToken(apiKey, secret, {apiRTC_UserAgent_Id: userId, ttl: 7200})).toJwt();
 console.log('token: ' + token);
 
 setTimeout(() => {
